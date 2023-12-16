ed854e3c0425a57b3ec4c954afddd902ee5ad31a
pablovazquezg <pablovazquezg@gmail.com>
2023-12-16 21:31:27 +0100

    feat: rot13 solution

--- katas/rot13.py
+++ katas/rot13.py
@@ -1,13 +1,18 @@
 # https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+# ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+# Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+# Please note that using encode is considered cheating.
+
 import string
 
 
 def rot13(message):
-    alphabet = string.ascii_lowercase + string.ascii_uppercase
-    rotated = alphabet[13:] + alphabet[:13]
-    table = str.maketrans(alphabet, rotated)
-    return message.translate(table).lower()
+    lc = string.ascii_lowercase[13:] + string.ascii_lowercase[:13]
+    uc = string.ascii_uppercase[13:] + string.ascii_uppercase[:13]
+    table = str.maketrans(string.ascii_letters, lc + uc)
+    return message.translate(table)
 
 
-text = "Test"
-print(rot13(text))  # Prints grfg
+# Example usage:
+print(rot13("Hello, World!"))  # Output: "Uryyb, Jbeyq!"
+print(rot13("ROT13 example!"))  # Output: "EBG13 rknzcyr!"
