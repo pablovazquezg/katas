0aba3725d260f49c9a46efd87473624350c6bb1f
pablovazquezg <pablovazquezg@gmail.com>
2023-12-17 11:28:11 +0100

    admin: added clarifying comment

--- katas/best_travel.py
+++ katas/best_travel.py
@@ -17,4 +17,4 @@ def choose_best_sum(
 
 
 xs = [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]
-print(choose_best_sum(230, 4, xs))
+print(choose_best_sum(230, 4, xs))  # prints 230
